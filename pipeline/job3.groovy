fa3b1d3d527f11c04252a18dbc563ea92dbee87d
moudrick <moudrick@gmail.com>
2023-06-03 11:44:31 +0300

    Create job3.groovy

---
+++ pipeline/job3.groovy
@@ -0,0 +1,48 @@
+pipeline {
+agent any
+    
+    environment {
+        REPO = 'https://github.com/moudrick/kbot'
+        BRANCH = 'main'
+        
+    }
+    stages {
+        stage('clone') {
+            steps {
+                echo 'CLONE REPOSITORY'
+                get branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+        stage('test') {
+            steps {
+                echo 'TEST EXECUTION STARTED'
+                sh 'make test'
+            }
+        }
+        stage('build') {
+            steps {
+                echo 'BUILD EXECUTION STARTED'
+                sh 'make build'
+            }
+        }
+        stage('image') {
+            steps {
+                script {
+                    echo 'IMAGE BUILD STARTED'
+                    sh 'make image'
+                }
+            }
+        }
+        stage('push') {
+            steps {
+                script {
+                    
+                    echo 'PUSH BUILD STARTED'
+                    docker.withRegistry('', '.dockerhub') {
+                    sh 'make push'
+                    }
+                }
+            }
+        }
+    }
+}
